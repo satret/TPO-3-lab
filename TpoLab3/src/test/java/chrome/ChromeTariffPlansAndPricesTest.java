1e3a5ad1f3737e8900e2749bf997f95259ff5ed2
satret@niuitmo.ru <satret@niuitmo.ru>
2024-05-29 02:29:39 +0300

    add 3 test

--- TpoLab3/src/test/java/chrome/ChromeTariffPlansAndPricesTest.java
+++ TpoLab3/src/test/java/chrome/ChromeTariffPlansAndPricesTest.java
@@ -16,8 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChromeTariffPlansAndPricesTest {
     private RemoteWebDriver driver;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
 
     @BeforeEach
     public void setUp() {
